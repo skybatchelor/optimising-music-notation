a7840f848248f76064d3d25a9d4d2c0eefe9ca22
SkyBatchelor <skybatchelor@yahoo.com>
2024-02-09 14:38:43 +0000

    OMN-22: get updated Chord class from other branch

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/staveelements/Chord.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/staveelements/Chord.java
@@ -2,6 +2,7 @@ package uk.ac.cam.optimisingmusicnotation.representation.staveelements;
 
 import uk.ac.cam.optimisingmusicnotation.rendering.MusicCanvas;
 import uk.ac.cam.optimisingmusicnotation.representation.properties.Accidental;
+import uk.ac.cam.optimisingmusicnotation.representation.properties.MusicalPosition;
 import uk.ac.cam.optimisingmusicnotation.representation.properties.Pitch;
 import uk.ac.cam.optimisingmusicnotation.representation.properties.RenderingConfiguration;
 import uk.ac.cam.optimisingmusicnotation.representation.staveelements.chordmarkings.ChordMarking;
@@ -9,21 +10,48 @@ import uk.ac.cam.optimisingmusicnotation.representation.staveelements.chordmarki
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Chord extends BeamGroup {
+public class Chord extends BeamGroup {
     protected final List<Note> notes;
     protected final List<ChordMarking> markings;
+    protected final MusicalPosition musicalPosition;
+    protected final float durationInCrochets;
+    protected final NoteType noteType;
 
-    public Chord(){
+    public Chord() {
         notes = new ArrayList<>();
         markings = new ArrayList<>();
+        musicalPosition = new MusicalPosition(null, 0);
+        durationInCrochets = 0;
+        noteType = NoteType.BREVE;
     };
+
+    public Chord(List<Pitch> pitches, List<Accidental> accidentals, MusicalPosition musicalPosition, float durationInCrochets, NoteType noteType) {
+        notes = new ArrayList<>(pitches.size());
+        for (int i = 0; i < pitches.size(); ++i) {
+            notes.add(new Note(pitches.get(i), accidentals.get(i)));
+        }
+        markings = new ArrayList<>();
+        this.musicalPosition = musicalPosition;
+        this.durationInCrochets = durationInCrochets;
+        this.noteType = noteType;
+    }
+
+    public void addMarking(ChordMarking marking) {
+        markings.add(marking);
+    }
+
     @Override
     public <Anchor> void draw(MusicCanvas<Anchor> canvas, RenderingConfiguration config) {
 
     }
 
-    private static abstract class Note {
+    private static class Note {
         Pitch pitch;
         Accidental accidental;
+
+        public Note(Pitch pitch, Accidental accidental) {
+            this.pitch = pitch;
+            this.accidental = accidental;
+        }
     }
 }
