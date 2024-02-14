70f23ed1a4c7b669d50b5edac13310ef203ed603
ClaireCooper <cjc233@cam.ac.uk>
2024-02-14 15:30:45 +0000

    OMN-20. Properly render pulse lines in rests

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/beatlines/BeatLine.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/beatlines/BeatLine.java
@@ -5,15 +5,24 @@ import uk.ac.cam.optimisingmusicnotation.representation.properties.MusicalPositi
 import uk.ac.cam.optimisingmusicnotation.representation.properties.RenderingConfiguration;
 
 public class BeatLine implements PulseLine {
+    @Override
+    public MusicalPosition getMusicalPosition() {
+        return musicalPosition;
+    }
 
-    MusicalPosition musicalPosition;
+    private MusicalPosition musicalPosition;
 
     public BeatLine(MusicalPosition musicalPosition) {
         this.musicalPosition = musicalPosition;
     }
 
-    public <Anchor> void draw(MusicCanvas<Anchor> canvas, RenderingConfiguration config) {
+    public <Anchor> void drawAboveStave(MusicCanvas<Anchor> canvas, RenderingConfiguration config) {
         Anchor startAnchor = canvas.getAnchor(musicalPosition);
         canvas.drawLine(startAnchor,0f,2f,0f,0f,RenderingConfiguration.pulseLineWidth);
+    }
+
+    public <Anchor> void drawFull(MusicCanvas<Anchor> canvas, RenderingConfiguration config) {
+        Anchor startAnchor = canvas.getAnchor(musicalPosition);
+        canvas.drawLine(startAnchor,0f,2f,0f,-4f,RenderingConfiguration.pulseLineWidth);
     }
 }
--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/whitespaces/Rest.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/whitespaces/Rest.java
@@ -2,6 +2,7 @@ package uk.ac.cam.optimisingmusicnotation.representation.whitespaces;
 
 import uk.ac.cam.optimisingmusicnotation.rendering.MusicCanvas;
 import uk.ac.cam.optimisingmusicnotation.representation.Line;
+import uk.ac.cam.optimisingmusicnotation.representation.beatlines.PulseLine;
 import uk.ac.cam.optimisingmusicnotation.representation.properties.MusicalPosition;
 import uk.ac.cam.optimisingmusicnotation.representation.properties.RenderingConfiguration;
 
@@ -19,10 +20,17 @@ public class Rest implements Whitespace {
     public <Anchor> void draw(MusicCanvas<Anchor> canvas, Line line, RenderingConfiguration config) {
         canvas.drawWhitespace(canvas.getAnchor(startMusicalPosition), 0, 0.5f,
                 canvas.getAnchor(endMusicalPosition), 0, -4.5f);
+        MusicalPosition pulsePosition;
+        for (PulseLine p: line.getPulseLines()) {
+            pulsePosition = p.getMusicalPosition();
+            if(pulsePosition.compareTo(startMusicalPosition)>=0 && pulsePosition.compareTo(endMusicalPosition)<=0){
+                p.drawFull(canvas,config);
+            }
+        }
 
-        for (int i = (int) Math.ceil(startMusicalPosition.crotchetsIntoLine()); i <= endMusicalPosition.crotchetsIntoLine(); i++) {
-            MusicalPosition startPosition = new MusicalPosition(line, i);
-            Anchor startAnchor = canvas.getAnchor(startPosition);
-            canvas.drawLine(startAnchor,0f,2f,0f,-4f,RenderingConfiguration.pulseLineWidth);}
+//        for (int i = (int) Math.ceil(startMusicalPosition.crotchetsIntoLine()); i <= endMusicalPosition.crotchetsIntoLine(); i++) {
+//            MusicalPosition startPosition = new MusicalPosition(line, i);
+//            Anchor startAnchor = canvas.getAnchor(startPosition);
+//            canvas.drawLine(startAnchor,0f,2f,0f,-4f,RenderingConfiguration.pulseLineWidth);}
     }
 }
