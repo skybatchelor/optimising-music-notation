b9641b43e733a49a78b5f1fe66e6f03973e5728d
SkyBatchelor <skybatchelor@yahoo.com>
2024-02-16 11:30:08 +0000

    OMN-20: colour and position of pulse lines

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/beatlines/BeatLine.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/beatlines/BeatLine.java
@@ -4,15 +4,17 @@ import uk.ac.cam.optimisingmusicnotation.rendering.MusicCanvas;
 import uk.ac.cam.optimisingmusicnotation.representation.properties.MusicalPosition;
 import uk.ac.cam.optimisingmusicnotation.representation.properties.RenderingConfiguration;
 
+import java.awt.*;
+
 public class BeatLine implements PulseLine {
     @Override
     public MusicalPosition getMusicalPosition() {
         return musicalPosition;
     }
 
-    private MusicalPosition musicalPosition;
+    private final MusicalPosition musicalPosition;
 
-    private int beatWeight;
+    private final int beatWeight;
 
     public BeatLine(MusicalPosition musicalPosition, int beatWeight) {
         this.musicalPosition = musicalPosition;
@@ -22,11 +24,11 @@ public class BeatLine implements PulseLine {
 
     public <Anchor> void drawAboveStave(MusicCanvas<Anchor> canvas, RenderingConfiguration config) {
         Anchor startAnchor = canvas.getAnchor(musicalPosition);
-        canvas.drawLine(startAnchor,0f,2f,0f,0f,RenderingConfiguration.pulseLineWidth);
+        canvas.drawLine(startAnchor,0f,2f,0f,0.25f,RenderingConfiguration.pulseLineWidth, new Color(0xCCCCCC));
     }
 
     public <Anchor> void drawFull(MusicCanvas<Anchor> canvas, RenderingConfiguration config) {
         Anchor startAnchor = canvas.getAnchor(musicalPosition);
-        canvas.drawLine(startAnchor,0f,2f,0f,-4f,RenderingConfiguration.pulseLineWidth);
+        canvas.drawLine(startAnchor,0f,0.5f,0f,-4f,RenderingConfiguration.pulseLineWidth, new Color(0xCCCCCC));
     }
 }
