34a36bd23b6f570fa772aad5626d0a5163412f66
SkyBatchelor <skybatchelor@yahoo.com>
2024-02-16 11:56:36 +0000

    change fields in RenderingConfiguration from static to part of the record (part 2 because I forgot to actually commit the config file)

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/properties/RenderingConfiguration.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/properties/RenderingConfiguration.java
@@ -1,5 +1,4 @@
 package uk.ac.cam.optimisingmusicnotation.representation.properties;
 
-public record RenderingConfiguration() {
-    public static float pulseLineWidth = 0.1f;
+public record RenderingConfiguration(float pulseLineWidth, boolean upwardStems) {
 }
