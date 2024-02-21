fda91372ad1a6611d343c4fc1adf1ce80a4a423c
SkyBatchelor <skybatchelor@yahoo.com>
2024-02-21 13:16:59 +0000

    OMN-20: thick bar lines

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/beatlines/BarLine.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/beatlines/BarLine.java
@@ -5,6 +5,8 @@ import uk.ac.cam.optimisingmusicnotation.representation.properties.MusicalPositi
 import uk.ac.cam.optimisingmusicnotation.representation.properties.RenderingConfiguration;
 import uk.ac.cam.optimisingmusicnotation.representation.properties.TimeSignature;
 
+import java.awt.*;
+
 public class BarLine implements PulseLine {
     @Override
     public MusicalPosition getMusicalPosition() {
@@ -22,11 +24,11 @@ public class BarLine implements PulseLine {
 
     public <Anchor> void drawAboveStave(MusicCanvas<Anchor> canvas) {
         Anchor startAnchor = canvas.getAnchor(musicalPosition);
-        canvas.drawLine(startAnchor,0f,2f,0f,0f,RenderingConfiguration.pulseLineWidth);
+        canvas.drawLine(startAnchor,0f,2f,0f,0.25f,RenderingConfiguration.barLineWidth, new Color(0xCCCCCC));
     }
 
     public <Anchor> void drawFull(MusicCanvas<Anchor> canvas) {
         Anchor startAnchor = canvas.getAnchor(musicalPosition);
-        canvas.drawLine(startAnchor,0f,2f,0f,-4f,RenderingConfiguration.pulseLineWidth);
+        canvas.drawLine(startAnchor,0f,0.5f,0f,-4f,RenderingConfiguration.barLineWidth, new Color(0xCCCCCC));
     }
 }
--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/properties/RenderingConfiguration.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/properties/RenderingConfiguration.java
@@ -2,6 +2,7 @@ package uk.ac.cam.optimisingmusicnotation.representation.properties;
 
 public class RenderingConfiguration {
     public static float pulseLineWidth = 0.1f;
+    public static float barLineWidth = 0.4f;
     public static float stemWidth = 0.15f;
     public static boolean upwardStems = true;
 }
