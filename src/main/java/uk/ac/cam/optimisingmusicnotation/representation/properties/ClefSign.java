97d81946cbe80128400a63800678884cf9018985
ClaireCooper <cjc233@cam.ac.uk>
2024-02-26 14:37:57 +0000

    OMN-24. Fix clef problem with line number system

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/properties/ClefSign.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/properties/ClefSign.java
@@ -2,14 +2,14 @@ package uk.ac.cam.optimisingmusicnotation.representation.properties;
 
 public enum ClefSign {
     G(8,2,3),
-    F(3,4,2),
-    C(4,3,2),
-    PERCUSSION(2,3,1),
-    TAB(4,3,2);
-    public final int height;
+    F(3,6,2),
+    C(4,4,2),
+    PERCUSSION(2,6,1),
+    TAB(4,6,2);
+    public final float height;
     public final int defaultLinesFromBottomOfStave;
     public final int lineDistanceFromBottomOfClef;
-    ClefSign(int height,int defaultLinesFromBottomOfStave,int lineDistanceFromBottomOfClef) {
+    ClefSign(float height,int defaultLinesFromBottomOfStave,int lineDistanceFromBottomOfClef) {
         this.height = height;
         this.defaultLinesFromBottomOfStave = defaultLinesFromBottomOfStave;
         this.lineDistanceFromBottomOfClef = lineDistanceFromBottomOfClef;
