79b497e074c6c75f84e00965fa79285e7c138e55
Jack <yg410@cam.ac.uk>
2024-02-26 01:35:42 +0000

    OMN-41 Initial slur implementation

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/staveelements/musicgroups/Slur.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/staveelements/musicgroups/Slur.java
@@ -1,4 +1,27 @@
 package uk.ac.cam.optimisingmusicnotation.representation.staveelements.musicgroups;
 
-public class Slur {
+import uk.ac.cam.optimisingmusicnotation.rendering.MusicCanvas;
+import uk.ac.cam.optimisingmusicnotation.representation.properties.ChordAnchors;
+import uk.ac.cam.optimisingmusicnotation.representation.properties.RenderingConfiguration;
+import uk.ac.cam.optimisingmusicnotation.representation.staveelements.Chord;
+
+import java.util.List;
+import java.util.Map;
+
+public class Slur extends MusicGroup {
+    public Slur() {
+        super();
+    }
+
+    public Slur(List<Chord> chords) {
+        super(chords);
+    }
+
+    @Override
+    public <Anchor> void draw(MusicCanvas<Anchor> canvas, Map<Chord, ChordAnchors<Anchor>> chordAnchorsMap) {
+        // TODO: check if is on the same line
+        float absoluteYOffset = .5f; // set vertical offset from Notehead here
+        float signedYOffset = RenderingConfiguration.upwardStems ? -absoluteYOffset : absoluteYOffset;
+        canvas.drawCurve(chordAnchorsMap.get(firstChord).notehead(), 0, signedYOffset, chordAnchorsMap.get(lastChord).notehead(), 0, signedYOffset, .2f, RenderingConfiguration.upwardStems);
+    }
 }
