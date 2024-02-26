7c7a748737ec6d7bd666dd850f38ea945c4ec87a
Dron Hazra <dronh.to@gmail.com>
2024-02-26 22:46:08 +0000

    syncopation is fixed

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/staveelements/BeamGroup.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/staveelements/BeamGroup.java
@@ -84,17 +84,24 @@ public class BeamGroup implements StaveElement {
             }
 
             for (Beam beam : beams) {
+                float beamOffset = -(sign * beam.number * RenderingConfiguration.beamWidth
+                        + sign * RenderingConfiguration.gapBetweenBeams * beam.number);
                 if (beam.startIndex == beam.endIndex) {
-                    // canvas.drawBeam(chordAnchorsMap.get(chords.get(beam.startIndex)).stemEnd(),
-                    // -1, 3.125f - 1f * beam.number,
-                    // chordAnchorsMap.get(chords.get(beam.endIndex)).stemEnd(), 0, 3.125f - 0.8f *
-                    // beam.number,
-                    // 0.75f);
-                    System.out.println("Drawing beam with no end");
-                    // TODO: implement beamlets here?
+                    // TODO: fix beamlets
+                    if (beam.startIndex == 0) {
+                        Anchor leftAnchor = chordAnchorsMap.get(chords.get(beam.startIndex)).stemEnd();
+                        Anchor rightAnchor = canvas.interpolateAnchors(leftAnchor,
+                                chordAnchorsMap.get(chords.get(beam.startIndex + 1)).stemEnd(), 0.5f);
+                        canvas.drawBeam(leftAnchor, 0, beamOffset, rightAnchor, 0, beamOffset,
+                                RenderingConfiguration.beamWidth);
+                    } else if (beam.endIndex == chords.size() - 1) {
+                        Anchor rightAnchor = chordAnchorsMap.get(chords.get(beam.startIndex)).stemEnd();
+                        Anchor leftAnchor = canvas.interpolateAnchors(rightAnchor,
+                                chordAnchorsMap.get(chords.get(beam.endIndex - 1)).stemEnd(), 0.5f);
+                        canvas.drawBeam(leftAnchor, 0, beamOffset, rightAnchor, 0, beamOffset,
+                                RenderingConfiguration.beamWidth);
+                    }
                 } else {
-                    float beamOffset = -(sign * beam.number * RenderingConfiguration.beamWidth
-                            + sign * RenderingConfiguration.gapBetweenBeams * beam.number);
                     canvas.drawBeam(
                             chordAnchorsMap.get(chords.get(beam.startIndex)).stemEnd(),
                             0,
@@ -109,29 +116,5 @@ public class BeamGroup implements StaveElement {
         for (Chord chord : chords) {
             chord.draw(canvas, chordAnchorsMap);
         }
-
-//        if (chords.size() == 1) {
-//            Anchor note = chords.get(0).drawRetAnchor(canvas);
-//            canvas.drawBeam(note, -1, 3, 0, 3, 0.75f);
-//        } else {
-//            List<Anchor> anchors = new ArrayList<Anchor>();
-//            Anchor start = chords.get(0).drawRetAnchor(canvas);
-//            Anchor end = chords.get(chords.size() - 1).drawRetAnchor(canvas);
-//            anchors.add(start);
-//            canvas.drawBeam(start, 0, 3.125f, end, 0, 3.125f, 0.75f);
-//            for (Chord chord : chords.subList(1, chords.size() - 1)) {
-//                anchors.add(chord.drawRetAnchor(canvas));
-//            }
-//            anchors.add(end);
-//            for (Beam beam : beams) {
-//                if (beam.startIndex == beam.endIndex) {
-//                    canvas.drawBeam(anchors.get(beam.startIndex), -1, 3.125f - 1f * beam.number,
-//                            anchors.get(beam.endIndex), 0, 3.125f - 0.8f * beam.number, 0.75f);
-//                } else {
-//                    canvas.drawBeam(anchors.get(beam.startIndex), 0, 3.125f - 1f * beam.number,
-//                            anchors.get(beam.endIndex), 0, 3.125f - 1f * beam.number, 0.75f);
-//                }
-//            }
-//        }
     }
 }
