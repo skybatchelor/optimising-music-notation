77c126ff8f756ee4fa18e261a6479e2a565eae55
camak2441 <ak2441@cam.ac.uk>
2024-03-05 22:57:12 +0000

    Altered Dynamic to ensure that wider characters render correctly

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/staveelements/musicgroups/Dynamic.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/staveelements/musicgroups/Dynamic.java
@@ -26,7 +26,7 @@ public class Dynamic extends MusicGroup {
         Anchor anchor = canvas.getLowestStaveLineAnchor(musicalPosition);
         Anchor lowestAnchor = canvas.getMinAnchor(chords.stream().map((chord) -> chordAnchorsMap.get(chord).getLowestAnchor(canvas, chord)).toList(), anchor, canvas::isAnchorBelow);
         anchor = canvas.getTakeXTakeYAnchor(canvas.getAnchor(musicalPosition), lowestAnchor);
-        float width = text.length() * 1.5f;
+        float width = text.length() * 2f;
         try {
             canvas.drawText(RenderingConfiguration.dynamicsFontFilePath, text,10f, TextAlignment.CENTRE, anchor,
                     -width/2, RenderingConfiguration.dynamicsOffset + RenderingConfiguration.dynamicsTextHeight / 2, width, RenderingConfiguration.dynamicsTextHeight);
