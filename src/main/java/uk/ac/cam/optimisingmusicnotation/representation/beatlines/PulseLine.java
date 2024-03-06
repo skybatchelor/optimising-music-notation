ceced86078742d7785612cf49bf168a99f8cc362
camak2441 <ak2441@cam.ac.uk>
2024-03-06 15:41:10 +0000

    PulseLine documentation

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/beatlines/PulseLine.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/beatlines/PulseLine.java
@@ -12,21 +12,21 @@ public interface PulseLine {
 
     /**
      * Draws the pulse lines around the staves.
-     * @param canvas
-     * @param stave
-     * @param extendUp
-     * @param extendDown
-     * @param downLength
-     * @param drawLabel
-     * @param <Anchor>
+     * @param canvas the canvas used for rendering the score
+     * @param stave the stave to draw the pulse lines on
+     * @param extendUp whether to extend the pulse lines to meet the line above
+     * @param extendDown whether to extend the pulse lines below the stave
+     * @param downLength how far to extend down the pulse lines
+     * @param drawLabel whether to draw the label on the pulse line or not
+     * @param <Anchor> the anchor type the canvas uses
      */
     <Anchor> void drawAroundStave(MusicCanvas<Anchor> canvas, Stave stave, boolean extendUp, boolean extendDown, float downLength, boolean drawLabel);
 
     /**
      * Draws the pulse lines through whitespace.
-     * @param canvas
-     * @param stave
-     * @param <Anchor>
+     * @param canvas the canvas used for rendering the score
+     * @param stave the stave that is being drawn through
+     * @param <Anchor> the anchor type the canvas uses
      */
     <Anchor> void drawFull(MusicCanvas<Anchor> canvas, Stave stave);
 }
