4b56ace8784888921213b0dd3b19825160356dd8
ClaireCooper <cjc233@cam.ac.uk>
2024-03-06 13:10:23 +0000

    OMN-98. Delete annotations

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/staveelements/annotations/Annotation.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.cam.optimisingmusicnotation.representation.staveelements.annotations;
-
-import uk.ac.cam.optimisingmusicnotation.representation.staveelements.StaveElement;
-
-public abstract class Annotation implements StaveElement {
-
-}
