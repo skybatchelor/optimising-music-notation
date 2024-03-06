cb08572ec2c61a36fd6fea1a5836da518046b31b
camak2441 <ak2441@cam.ac.uk>
2024-03-06 17:42:41 +0000

    Pitch documentation

--- src/main/java/uk/ac/cam/optimisingmusicnotation/representation/properties/Pitch.java
+++ src/main/java/uk/ac/cam/optimisingmusicnotation/representation/properties/Pitch.java
@@ -2,9 +2,9 @@ package uk.ac.cam.optimisingmusicnotation.representation.properties;
 
 /**
  * Represents a pitch on the score.
- * @param rootStaveLine
- * @param semitonesAbove
- * @param semitonesAboveC0
+ * @param rootStaveLine the number of the stave or space of the pitch, with the lowest line in a stave as 0, and the first space above that as 1
+ * @param semitonesAbove the semitones above the base value of that line
+ * @param semitonesAboveC0 the semitones above tC0
  */
 public record Pitch (Integer rootStaveLine, Integer semitonesAbove, Integer semitonesAboveC0) {
 }
